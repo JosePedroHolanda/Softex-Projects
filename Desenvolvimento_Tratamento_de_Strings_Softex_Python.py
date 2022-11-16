2e254fa345b8ff62ff1ed0fc52f4db1f195e0218
JosePedroHolanda <109304836+JosePedroHolanda@users.noreply.github.com>
2022-11-16 20:33:59 -0300

    Create Desenvolvimento_Tratamento_de_Strings_Softex_Python.py

---
+++ Desenvolvimento_Tratamento_de_Strings_Softex_Python.py
@@ -0,0 +1,11 @@
+texto1 = 'Olá MUNDO!'
+resultado = texto1.lower()
+print(resultado)
+
+
+texto2 = 'Olá Mundo!'
+resultado = texto2.upper()
+print(resultado)
+
+texto3 = 'Olá mundo!'
+print(len(texto3))
